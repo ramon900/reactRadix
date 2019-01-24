dff33755f7ede38081ba770f9128b5248405551d
Aula Radix <radix@SJ208F08.local>
2019-01-24 10:59:11 -0200

    Agora Vai

--- desafioReact/src/screens/ListaScreen/index.js
+++ desafioReact/src/screens/ListaScreen/index.js
@@ -1,2 +1,38 @@
 import React from 'react';
 import {StyleSheet, View, FlatList, Text, TextInput} from 'react-native';
+
+class ListaScreen extends React.Component{
+
+    constructor(props){
+        super(props);
+    
+        this.initialTasks = [];
+    
+    this.state = {
+            initialTasks: [],
+            loading: false
+        };
+    }
+
+    render(){
+        return(
+            <View style={style.container}>
+            <TextInput 
+                placeholder = "Digite o filtro"
+            />
+            </View>
+            );
+        }
+    }
+    
+    const style = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#F5FCFF',
+            justifyContent: 'center'
+        }
+    });
+    
+    export default ListaScreen
+
+
