234143aabbf7e62321a56bdd29bca63a636fa6e2
Aula Radix <radix@SJ208F07.local>
2019-01-25 11:33:50 -0200

    mudanca besta

--- desafioReact/src/App.js
+++ desafioReact/src/App.js
@@ -7,13 +7,12 @@
  */
 
 import React, {Component} from 'react';
-//import {ListaScreen} from './screens';
-import CreateTaskScreen from './screens/CreateTaskScreen';//trocar pra lista screen
+import {ListaScreen} from './screens';
 
 export default class App extends Component {
   render() {
     return (
-      <CreateTaskScreen/>//trocar pra lista screen
+      <ListaScreen/>
     );
   }
 }
